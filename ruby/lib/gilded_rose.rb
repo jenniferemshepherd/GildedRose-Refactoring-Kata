f3792980cd26dda42e8df89ce8ffa60c02ebe741
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-28 22:25:37 +0000

    refactor to one-line if statements for item.quality > 0

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,9 +9,7 @@ class GildedRose
     @items.each do |item|
       unless item.name == "Sulfuras, Hand of Ragnaros"
         if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-              decrease_quality_by_1(item)
-          end
+          decrease_quality_by_1(item) if item.quality > 0
         else
           if item.quality < 50
             increment_quality_by_1(item)
@@ -21,9 +19,7 @@ class GildedRose
         item.sell_in -= 1
         if item.sell_in < 0
           if item.name != "Aged Brie"
-              if item.quality > 0
-                  decrease_quality_by_1(item)
-              end
+            decrease_quality_by_1(item) if item.quality > 0
             item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
           else
             increase_quality_by_1_not_beyond_limit(item)
