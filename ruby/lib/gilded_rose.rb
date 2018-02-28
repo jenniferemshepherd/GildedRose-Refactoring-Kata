6f8d0d46e032b1b5e1db0ab5b3a5394eb2dfb311
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-28 21:52:31 +0000

    introduce use of '-=' notation for negative increments

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -10,7 +10,7 @@ class GildedRose
       if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality -=
           end
         end
       else
@@ -20,13 +20,13 @@ class GildedRose
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
       end
       if item.sell_in < 0
         if item.name != "Aged Brie"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality -= item.quality
               end
             end
           item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
