d77ceb324e0dc2ae9867a747edb6333dee826bb8
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-01 13:18:31 +0000

    use constant instead of magic number
    ensure that all GR methods are wrapped within the GR class object

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,6 +1,10 @@
 require_relative 'item'
 
 class GildedRose
+
+  QUALITYLIMIT = 50
+  QUALITY_ALTER = 1
+
   def initialize(items)
     @items = items
   end
@@ -19,42 +23,42 @@ class GildedRose
       end
     end
   end
-end
 
-def item_that_increases_in_quality(item)
-  item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"
-end
-
-def decrease_sell_in(item)
-  item.sell_in -= 1
-end
-
-def increment_quality_by_1(item)
-  item.quality += 1
-end
-
-def decrease_quality_by_1(item)
-  item.quality -= 1
-end
-
-def increase_quality_by_1_not_beyond_limit(item)
-  increment_quality_by_1(item) if item.quality < 50
-end
-
-def increase_backstage_pass_quality(item)
-  if item.name == "Backstage passes to a TAFKAL80ETC concert"
-      increase_quality_by_1_not_beyond_limit(item) if item.sell_in < 11
-      increase_quality_by_1_not_beyond_limit(item) if item.sell_in < 6
+  def item_that_increases_in_quality(item)
+    item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"
   end
-end
 
-def past_sell_in(item)
-  if item.sell_in < 0
-    if item.name == "Aged Brie"
-      increase_quality_by_1_not_beyond_limit(item)
-    else
-      decrease_quality_by_1(item) if item.quality > 0
-      item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
+  def decrease_sell_in(item)
+    item.sell_in -= 1
+  end
+
+  def increment_quality_by_1(item)
+    item.quality += 1
+  end
+
+  def decrease_quality_by_1(item)
+    item.quality -= 1
+  end
+
+  def increase_quality_by_1_not_beyond_limit(item)
+    increment_quality_by_1(item) if item.quality < QUALITYLIMIT
+  end
+
+  def increase_backstage_pass_quality(item)
+    if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        increase_quality_by_1_not_beyond_limit(item) if item.sell_in < 11
+        increase_quality_by_1_not_beyond_limit(item) if item.sell_in < 6
+    end
+  end
+
+  def past_sell_in(item)
+    if item.sell_in < 0
+      if item.name == "Aged Brie"
+        increase_quality_by_1_not_beyond_limit(item)
+      else
+        decrease_quality_by_1(item) if item.quality > 0
+        item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      end
     end
   end
 end
