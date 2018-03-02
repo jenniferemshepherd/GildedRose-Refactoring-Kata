ce02d44c437901a7b34cab13164de00a32275ad2
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-02 19:02:33 +0000

    remove methods that are not needed

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -9,12 +9,6 @@ require 'gilded_rose'
 
 describe GildedRose do
   describe "#update_quality" do
-    # it "does not change the name" do
-    #   items = [Item.new("foo", 0, 0)]
-    #   GildedRose.new(items).update_quality()
-    #   expect(items[0].name).to eq "foo"
-    # end
-
     it "behaves as predicted" do
       items = [Sulfuras.new(0, 80),
               AgedBrie.new(2,0),
