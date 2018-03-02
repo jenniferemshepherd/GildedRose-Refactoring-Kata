df456ada416ce694b9632eed68133d236056bbf2
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-02 18:34:13 +0000

    rename update method for Elixir

--- ruby/spec/elixir_spec.rb
+++ ruby/spec/elixir_spec.rb
@@ -3,19 +3,19 @@ require 'elixir'
 describe Elixir do
   it "decreases in quality" do
     item = Elixir.new(4, 6)
-    item.update_item
+    item.update
     expect(item.quality).to eq 5
   end
 
   it "decreases sell_in" do
     item = Elixir.new(4, 6)
-    item.update_item
+    item.update
     expect(item.sell_in).to eq 3
   end
 
   it "decreases in quality twice as fast when sell_in<0" do
     item = Elixir.new(-1, 6)
-    item.update_item
+    item.update
     expect(item.quality).to eq 4
   end
 end
